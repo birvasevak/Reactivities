0c87161052e0c01009d0730c8d733a76d67b9593
Sevak, Birva <birvasevak@gmail.com>
2024-04-27 13:09:39 -0400

    added axios for http client

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
 
   return (
-    <h1>Reactivities</h1>
-  )
+    <div>
+      <h1>Reactivities</h1>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
