9f0dc4eeb9892031f617cba1129a2631f96f8de1
Sevak, Birva <birvasevak@gmail.com>
2024-04-29 23:02:30 -0400

    created activity list and details components for dashboard

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { Activity } from "../../../app/models/Activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+
+interface Props {
+  activities: Activity[];
+}
+export default function ActivityDashboard({ activities }: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities}></ActivityList>
+      </Grid.Column>
+      <Grid.Column width="6">
+        {activities[0] && (
+          <ActivityDetails activity={activities[0]}></ActivityDetails>
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,31 @@
+import { Activity } from "../../../app/models/Activity";
+import { Card, Image, Button } from "semantic-ui-react";
+
+interface Props {
+  activity: Activity;
+}
+
+export default function ActivityDetails({ activity }: Props) {
+  return (
+    <Card fluid>
+      <Image
+        src={`/assets/categoryImages/${activity.category}.jpg`}
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span className="date">{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths="2">
+          <Button basic color="blue" content="Edit" />
+          <Button basic color="grey" content="Cancel" />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+}
