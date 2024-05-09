108cb444fb0414f8b96769340102806eb2bfe3b0
Sevak, Birva <birvasevak@gmail.com>
2024-05-09 19:32:05 -0400

    configure MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,18 @@
+import { observer } from "mobx-react-lite";
 import { Fragment, useEffect, useState } from "react";
-import { Container } from "semantic-ui-react";
+import { Button, Container } from "semantic-ui-react";
 import { v4 as uuid } from "uuid";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import agents from "../api/agent";
 import { Activity } from "../models/Activity";
+import { useStore } from "../stores/store";
 import LoadingComponent from "./LoadingComponent";
 import NavBar from "./NavBar";
 import "./styles.css";
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
@@ -84,6 +88,12 @@ function App() {
     <Fragment>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: "7em" }}>
+        <h2>{activityStore.title}</h2>
+        <Button
+          content="Add exclamation!"
+          positive
+          onClick={activityStore.setTitle}
+        ></Button>
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
@@ -101,4 +111,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
