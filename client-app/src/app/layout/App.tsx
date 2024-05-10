1ac2e6252080e0b6051227f910493d9248fd507e
Sevak, Birva <birvasevak@gmail.com>
2024-05-10 09:40:43 -0400

    configured react router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,16 @@
 import { observer } from "mobx-react-lite";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 import { Container } from "semantic-ui-react";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useStore } from "../stores/store";
-import LoadingComponent from "./LoadingComponent";
 import NavBar from "./NavBar";
 import "./styles.css";
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) {
-    return <LoadingComponent content="Loading app" />;
-  }
   return (
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard></ActivityDashboard>
+        <Outlet />
       </Container>
     </Fragment>
   );
